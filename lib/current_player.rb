4024c7ec6667b8f2acd0fe70a8e66a6cdf99c045
phamducv <phamducv@gmail.com>
2015-11-20 16:30:04 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,22 @@
+def turn_count(board)
+        counter=0
+  board.each do |board|
+    if board!=" "
+
+            counter+=1
+  end
+end
+          return counter
+end
+
+
+
+def current_player(board)
+        if turn_count(board)==1
+                return "O"
+        elsif turn_count(board) % 2
+                return "X"
+        else
+                return "O"
+        end
+end
